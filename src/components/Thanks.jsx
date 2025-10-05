8e6f62c3ae17178c63c71e51be007cffc0cb7375
leon2192 <leon.strokes@gmail.com>
2025-10-05 19:01:17 -0300

    fc

--- src/components/Thanks.jsx
+++ src/components/Thanks.jsx
@@ -5,8 +5,8 @@ const Thanks = () => {
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
 
-  const desktopImage = "/images/005/gracias-nuevo.png";
-  const mobileImage = "/images/005/grac.png";
+  const desktopImage = "/images/modigicaciones/gracias.webp";
+  const mobileImage = "/images/modificaciones/gracias.webp";
 
   return (
     <Box
